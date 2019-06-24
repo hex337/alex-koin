0ebaecfca40f85125f4ac9a3086bc65abd3a82a8
Alex Kleissner <alexk@chime.com>
2019-06-23 23:14:59 -0700

    Fact was too loose, be more specific now

--- lib/alex_koin/slack.ex
+++ lib/alex_koin/slack.ex
@@ -50,16 +50,16 @@ defmodule AlexKoin.SlackRtm do
 
   defp message_type(text) do
     cond do
+      text == "fact" -> {:fact, text}
       text =~ "create koin" -> {:create, text}
       text =~ "my balance" -> {:balance, text}
       text =~ "balance for" -> {:other_balance, text}
       text =~ "transfer" -> {:transfer, text}
       text =~ "list koins" -> {:list_koins, text}
       text =~ "leaderboard" -> {:leaderboard, text}
-      text =~ "fact" -> {:fact, text}
-      text =~ "reconcile" -> {:reconcile, text}
       text =~ "announce" -> {:announce, text}
       text =~ "display" -> {:display, text}
+      text =~ "reconcile" -> {:reconcile, text}
       true -> {:nothing, text}
     end
   end
