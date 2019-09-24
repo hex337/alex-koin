05d926828b93bfd83494dc96e94b791cd8359ede
Alex Kleissner <alexk@chime.com>
2019-09-24 16:07:09 -0700

    One more fix for help command.

--- lib/alex_koin/commands/help.ex
+++ lib/alex_koin/commands/help.ex
@@ -5,7 +5,7 @@ defmodule AlexKoin.Commands.Help do
 
   def execute(message, slack) do
     koin_lord_names = String.split(@koin_lord_ids, ",")
-                      |> Enum.map(fn id -> SlackDataHelpers.name_to_display_from_slack_id(id, slack.profiles) end)
+                      |> Enum.map(fn id -> SlackDataHelpers.name_to_display_from_slack_id(id, slack.users) end)
 
     msg = "Any questions about Alex Koin can be directed to the Lords of Koin: #{Enum.join(koin_lord_names, ", ")}"
     {msg, SlackDataHelpers.message_ts(message)}
