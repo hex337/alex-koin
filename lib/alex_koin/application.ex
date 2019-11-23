6f82717d1576783337ac5e0c1a9755745661797e
Allen Kerr <akerr@chime.com>
2019-11-22 16:01:41 -0800

    Don't load slackbot in tests

--- lib/alex_koin/application.ex
+++ lib/alex_koin/application.ex
@@ -6,21 +6,22 @@ defmodule AlexKoin.Application do
   def start(_type, _args) do
     import Supervisor.Spec
 
-    slack_token = Application.get_env(:alex_koin, AlexKoin.Slack)[:token]
-
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
       supervisor(AlexKoin.Repo, []),
       # Start the endpoint when the application starts
-      supervisor(AlexKoinWeb.Endpoint, []),
-      # Start your own worker by calling: AlexKoin.Worker.start_link(arg1, arg2, arg3)
-      # worker(AlexKoin.Worker, [arg1, arg2, arg3]),
-      %{
-        id: Slack.Bot,
-        start: { Slack.Bot, :start_link, [AlexKoin.SlackRtm, [], slack_token] }
-      }
+      supervisor(AlexKoinWeb.Endpoint, [])
     ]
+
+    children =
+      if System.get_env("MIX_ENV") == "test" do
+        children
+      else
+        slack_token = Application.get_env(:alex_koin, AlexKoin.Slack)[:token]
+        slackbot_supervisor = supervisor(Slack.Bot, [AlexKoin.SlackRtm, [], slack_token])
+        [slackbot_supervisor | children]
+      end
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
