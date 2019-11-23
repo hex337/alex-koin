c5cde2ad870395e05337320e3d5c1caa7c2c69a6
Allen Kerr <akerr@chime.com>
2019-11-22 16:30:02 -0800

    simiplify children setup

--- lib/alex_koin/application.ex
+++ lib/alex_koin/application.ex
@@ -7,21 +7,7 @@ defmodule AlexKoin.Application do
     import Supervisor.Spec
 
     # Define workers and child supervisors to be supervised
-    children = [
-      # Start the Ecto repository
-      supervisor(AlexKoin.Repo, []),
-      # Start the endpoint when the application starts
-      supervisor(AlexKoinWeb.Endpoint, [])
-    ]
-
-    children =
-      if System.get_env("MIX_ENV") == "test" do
-        children
-      else
-        slack_token = Application.get_env(:alex_koin, AlexKoin.Slack)[:token]
-        slackbot_supervisor = supervisor(Slack.Bot, [AlexKoin.SlackRtm, [], slack_token])
-        [slackbot_supervisor | children]
-      end
+    children = "MIX_ENV" |> System.get_env() |> get_children()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
@@ -34,5 +20,24 @@ defmodule AlexKoin.Application do
   def config_change(changed, _new, removed) do
     AlexKoinWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  defp get_children("test") do
+    [
+      # Start the Ecto repository
+      supervisor(AlexKoin.Repo, []),
+      # Start the endpoint when the application starts
+      supervisor(AlexKoinWeb.Endpoint, [])
+    ]
+  end
+
+  defp get_children(_) do
+    [slackbot_supervisor() | get_children("test")]
+  end
+
+  defp slackbot_supervisor do
+    slack_token = Application.get_env(:alex_koin, AlexKoin.Slack)[:token]
+
+    supervisor(Slack.Bot, [AlexKoin.SlackRtm, [], slack_token])
   end
 end
