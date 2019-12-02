f3c4b420df0586124b130ffd720a81289c6020b2
Allen Kerr <akerr@chime.com>
2019-12-01 16:25:17 -0800

    nuke fixtures in favor of factories

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,7 +2,7 @@ defmodule AlexKoin.Factory do
   use ExMachina.Ecto, repo: AlexKoin.Repo
 
   alias AlexKoin.Coins.Coin
-  alias AlexKoin.Account.{Wallet, User}
+  alias AlexKoin.Account.{Transaction, User, Wallet}
 
   def coin_factory(params) do
     {user, params} = Map.pop_lazy(params, :user, fn -> build(:user) end)
@@ -12,7 +12,7 @@ defmodule AlexKoin.Factory do
       origin: Faker.Lorem.sentence(),
       user: user,
       hash: UUID.uuid1(),
-      created_by_user_id: user.id,
+      created_by_user: user,
       wallet_id: wallet.id
     }
     |> merge_attributes(params)
@@ -31,6 +31,13 @@ defmodule AlexKoin.Factory do
       first_name: Faker.Name.first_name(),
       last_name: Faker.Name.last_name(),
       slack_id: sequence(:slack_id, fn x -> "#{Faker.Name.first_name()}_#{x}" end)
+    }
+  end
+
+  def transaction_factory do
+    %Transaction{
+      amount: 120.5,
+      memo: "some memo"
     }
   end
 end
