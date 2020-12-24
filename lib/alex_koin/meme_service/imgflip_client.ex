4aa40801980a3621dc6ee57e121b944aec98833f
Ziad Abdo <zabdo@chime.com>
2020-12-23 16:27:38 -0800

    Implement the caption_image piece of the meme client

--- lib/alex_koin/meme_service/imgflip_client.ex
+++ lib/alex_koin/meme_service/imgflip_client.ex
@@ -1,9 +1,20 @@
-defmodule Meme do 
+defmodule Meme do
   @derive [Poison.Encoder]
   defstruct [:id, :name, :url, :width, :height, :box_count]
 end
 
-defmodule ImgFlipClient do 
+defmodule MemeResponseBody do
+  @derive [Poison.Encoder]
+  defstruct [:success, data: %{memes: %Meme{}}]
+  #Decoding with the above syntax also does not work, which is a shame.
+end
+
+defmodule CaptionResponse do
+  @derive [Poison.Encoder]
+  defstruct [:success, data: %{url: String, page_url: String, error_message: String}]
+end
+
+defmodule ImgFlipClient do
   @moduledoc """
     Allows for fetching meme templates and captioning them
   """
@@ -12,15 +23,32 @@ defmodule ImgFlipClient do
 
   def fetch_template_list do
     case HTTPoison.get(@url <> "get_memes") do
-      # Below would have been cool to try out but body is not actually decrypted yet
-      # {:ok, %HTTPoison.Response{status_code: 200, body: %{"success" => true} = resp_body}} ->
       {:ok, %HTTPoison.Response{status_code: 200, body: resp_body}} ->
         decrypted = Poison.decode!(resp_body)
-        Enum.map(decrypted["data"]["memes"], fn data -> 
+        Enum.map(decrypted["data"]["memes"], fn data ->
           IO.inspect(data)
-          # This is just silly, I'm re serializing to turn it into an object :|
           Poison.decode!(Poison.encode!(data), as: %Meme{})
         end)
     end
+  end
+
+  def caption_image(template_id, text_top, text_bottom) do
+    post_params = build_caption_post_params(template_id, text_top, text_bottom)
+    case HTTPoison.post(@url <> "caption_image?" <> post_params, "") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: resp_body}} ->
+        Poison.decode!(resp_body, as: %CaptionResponse{})
+    end
+  end
+
+  def caption_image(_template_id, _boxes) do
+    ## Some meme templates have more than two placements for text. We can implement that too, if we want.
+  end
+
+  def build_caption_post_params(template_id, text_top, text_bottom) do
+    %{
+      template_id: template_id, username: System.get_env("IMGFLIP_USER"), password: System.get_env("IMGFLIP_PASS"),
+      text0: text_top, text1: text_bottom
+    }
+    |> URI.encode_query
   end
 end
